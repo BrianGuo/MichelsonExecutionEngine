5f7f635b4ef1eae7667fb9e64eff9bac38080c9d
Brian Guo <brianguosh@gmail.com>
2019-04-23 17:16:32 -0400

    [add] changing compiler version to avoid base package compile error

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
+opam install dependencies/vendors/ocaml-blake2 dependencies/vendors/ocaml-hacl/ dependencies/vendors/ocaml-secp256k1/ dependencies/vendors/ocaml-uecc/ dependencies/vendors/ocplib-json-typed/
 opam install dependencies/lib_stdlib/ dependencies/lib_crypto dependencies/lib_data_encoding/ dependencies/lib_error_monad/ dependencies/lib_micheline 
 opam install .
